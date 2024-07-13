ba2c06bd0b6cb687c199fcfc656a1cfadddf6f18
dag7dev <dag7@protonmail.com>
2024-07-13 20:26:46 +0200

    feat: no longer close due to inactivity

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,44 @@ import 'package:cpr/screens/compressioni_screen.dart';
 import 'package:cpr/screens/ventilazioni_screen.dart';
 import 'package:cpr/screens/pre_compressioni_screen.dart';
 import 'package:cpr/screens/chiama_ambulanza_screen.dart';
-
 import 'screens/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:wakelock_plus/wakelock_plus.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+    WakelockPlus.enable(); // Mantieni il display attivo all'avvio
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    WakelockPlus.disable(); // Disattiva wakelock quando l'app va in background
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.paused) {
+      WakelockPlus.disable(); // Disattiva quando l'app va in background
+    } else if (state == AppLifecycleState.resumed) {
+      WakelockPlus.enable(); // Riattiva quando l'app torna in primo piano
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
